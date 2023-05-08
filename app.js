1ab66bc8eb39b86d338c2f8693dafe5a5baa4083
generalgorilla195 <132958322+generalgorilla195@users.noreply.github.com>
2023-05-08 23:47:26 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var port = process.env.PORT || 8080;
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins 
-    requireHeader: ['referer'],
-    removeHeaders: []
+    requireHeader: [],
+    removeHeaders: [],
+    setHeaders: {"referer": "https://revisionvillage.com"}
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
